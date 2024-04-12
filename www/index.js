68cbdfecc4221a23952e61b3988f598c09d451eb
Daniel Dormann <dormann@fluur.de>
2024-04-12 21:14:13 +0200

    Use setInterval for slower fps rendering

--- www/index.js
+++ www/index.js
@@ -2,12 +2,18 @@ import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
 const containerNode = document.getElementById('universe-container');
-const universe = Universe.new(96, 96);
+const universe = Universe.new(64, 64);
 const width = universe.width();
 const height = universe.height();
 
 
 const renderLoop = () => {
+  renderUniverse();
+  
+  universe.tick();
+}
+
+const renderUniverse = () => {
   const cellsPtr = universe.cells();
   const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
@@ -17,22 +23,17 @@ const renderLoop = () => {
     for (let col = 0; col < width; col++) {
       const id = getIndex(row, col);
       
-      renderedUniverse += cells[id] === Cell.Alive
-        ? '◼'
-        : ' ';
+      renderedUniverse += cells[id] === Cell.Alive ? '◼' : ' ';
     }
 
     renderedUniverse += '\n';
   }
 
   containerNode.textContent = renderedUniverse;
-  
-  universe.tick();
-  requestAnimationFrame(renderLoop);
 }
 
 const getIndex = (row, col) => {
   return row * width + col;
 }
 
-requestAnimationFrame(renderLoop);
+setInterval(renderLoop.bind(this), 1000 / 10);
